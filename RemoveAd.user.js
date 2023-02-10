4c190489fe84ac47707773bc9971fd4dc537c364
李贤斌 <xianbin.me@qq.com>
2023-02-10 09:53:48 +0800

    update:更新v0.3版本

--- RemoveAd.user.js
+++ RemoveAd.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         真·百度去除广告
 // @namespace    https://github.com/lixianbin1/Tampermonkey
-// @version      0.2
+// @version      0.3
 // @description  删除百度的垃圾广告
 // @author       lixianbin1
 // @match        *://*.baidu.com/*
@@ -21,20 +21,26 @@
         }
     }
      function clear(){
-        // 清除A类广告
+        // 清除A类(竞价广告)广告
         const Aadvert = document.querySelectorAll("a[data-click]")
         Aadvert.forEach(e=>{
             if(e.getAttribute('data-click')=='{"rsv_snapshot":"1"}'){
                 hide($(e).parents('div.new-pmd')[0])
             }
         })
-        //清除B类广告
+        //清除B类(右侧引流广告)广告
         const Badvert = document.querySelectorAll("div[posid]")
         Badvert.forEach(e=>{
             hide($(e)[0])
         })
+        //清除C类(品牌广告)广告
+        const Cadvert = $("span.tenon_pc_comp_brand_tip-text:contains('品牌广告')")
+        Cadvert.map((e,a)=>{
+            hide($(a).parents('div.new-pmd')[0])
+        })
+        
     }
-    // 循环定时，防止后面插入
+    //循环定时，防止后面插入
     const time = setInterval(()=>{
         clear()
     },100)
